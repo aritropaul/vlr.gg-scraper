7555b662f9ac8567097b73492846e9260ef1655e
Aritro Paul <aritro.paul61@gmail.com>
2021-07-06 18:28:10 -0400

    news improvements

--- src/news.py
+++ src/news.py
@@ -8,25 +8,55 @@ class News:
     """
 
     def news():
-        URL = 'https://www.vlr.gg/'
+        URL = 'https://www.vlr.gg/news'
         page = requests.get(URL)
 
         soup = BeautifulSoup(page.content, 'html.parser')
-        news = soup.find_all('div', class_="js-home-news")[0]
-        dates = news.find_all('div', class_="wf-label")
-        cards = news.find_all('div', class_="wf-card")
-        newsDict = []
-        for key in dates:
-            for value in cards:
-                newsLink = value.find_all('a', class_="news-item")
-                for newsItem in newsLink:
-                    news = {}
-                    title = newsItem.find_all('div', class_="news-item-title")[0].get_text().strip()
-                    link = newsItem.get('href')
-                    news["date"] = key.get_text().strip().split(" \n")[0]
-                    news["news"] = title
-                    news["id"] = link.split("/")[1].split("/")[0]
-                    newsDict.append(news)
-                cards.remove(value)
-                break
-        return newsDict
+        vlr_module = soup.find(id="wrapper").parent.find_all(
+            "a", class_="wf-module-item"
+        )
+
+        result = []
+        for module in vlr_module:
+
+            # Titles of articles
+            title = module.find(
+                "div",
+                attrs={"style": "font-weight: 700; font-size: 15px; line-height: 1.3;"},
+            ).text.strip()
+
+            # get descriptions of articles
+            desc = module.find(
+                "div",
+                attrs={
+                    "style": " font-size: 13px; padding: 5px 0; padding-bottom: 6px; line-height: 1.4;"
+                },
+            ).text.strip()
+
+            # get date of articles
+            date = module.find("div", class_="ge-text-light").get_text().strip()
+            date = date.replace("\u2022", "").strip()
+            date = date.strip().split("  ")[0]
+            # date = datetime.strptime(date, "%b %d, %Y")
+            # date = date.replace(tzinfo=date.tzinfo)
+            # date = date.isoformat()
+
+            # get author of articles
+            author = module.find("div", class_="ge-text-light").get_text().strip()
+            author = author.replace("\u2022", "").strip()
+            author = author.strip().split("  ")[1]
+            # remove "by" in author section
+            author = author.strip().split(" ")[1]
+
+            # get url_path of articles
+            url = module["href"]
+            result.append(
+                {
+                    "title": title,
+                    "description": desc,
+                    "date": date,
+                    "author": author,
+                    "link": url,
+                }
+            )
+        return result
